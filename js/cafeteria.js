2cd6b1b763acd517a9168d96ef07a17d5869fb23
DIEGO ROGERIO GOMES <diego.rgomes5@senacsp.edu.br>
2023-07-20 09:32:36 -0300

    Feature: Adição de estilos para mobile

--- js/cafeteria.js
+++ js/cafeteria.js
@@ -1,4 +1,4 @@
 
-document.getElementsByClassName("btn-mobile").onclick = ()=>{
+document.getElementsByClassName("btn-mobile")[0].onclick = ()=>{
         document.getElementsByTagName("nav")[0].style.marginLeft= "0px";
     }
